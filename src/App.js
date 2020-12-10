4e9ad53624c8a12f77978f9bfd8740fa2c59e8c9
Leopoldo Garcia <legarbo@gmail.com>
2020-12-10 18:13:03 -0500

    Added checkout page, checkout item component and updated cart to use selectors

--- src/App.js
+++ src/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 import { Switch, Route, Redirect } from 'react-router-dom'
 import { connect } from 'react-redux'
 
+import { createStructuredSelector } from 'reselect'
+
+import { selectCurrentUser } from './redux/user/user.selectors'
 import './App.sass'
 
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
+import CheckoutPage from './pages/checkout/checkout.component' 
 import Header from './components/header/header.component'
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils'
 import { setCurrentUser } from './redux/user/user.actions'
 
@@ -55,14 +60,15 @@ class App extends React.Component {
                 )
               }
             />
+            <Route exact path='/checkout' component={CheckoutPage} />
         </Switch>
       </div>
     )
   }
 }
 
-const mapStateToProps = ({ user }) => ({
-  currentUser: user.currentUser
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
 })
 
 const mapDispatchToProps = dispatch => ({
--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -2,6 +2,13 @@ import { createSelector } from 'reselect'
 
 const selectCart = state => state.cart
 
+
+export const selectCartHidden = createSelector(
+  [selectCart],
+  cart => cart.hidden
+)
+
+
 export const selectCartItems = createSelector(
   [selectCart],
   cart => cart.cartItems
@@ -14,4 +21,13 @@ export const selectCartItemsCount = createSelector(
     (accumulatedQuantity, cartItem) =>
     accumulatedQuantity + cartItem.quantity, 0
   )
+)
+
+export const selectCartTotal = createSelector(
+  [selectCartItems],
+  cartItems =>
+    cartItems.reduce(
+      (accumulatedQuantity, cartItem) =>
+        accumulatedQuantity + cartItem.quantity * cartItem.price, 0
+    )
 )
